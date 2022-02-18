78cd235f2e675dda5f51d2dc3b8c70e31bcd2d0f
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-02-18 06:58:19 +0530

    let-const

---
+++ js-topics/let-const-var.js
@@ -0,0 +1,8 @@
+console.log(num);//undefined
+var num=10;
+
+console.log(let_num);//  Cannot access 'let_num' before initialization
+let let_num=10;
+
+console.log(const_num)// Cannot access 'const_num' before initialization
+const_num=10;
