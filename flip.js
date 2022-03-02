51fdda172faa1bc960d1ce2d0d4460b52d581e43
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-03-02 09:27:27 +0530

    updated

---
+++ flip.js
@@ -0,0 +1,17 @@
+/* approch1
+    function flip(input){
+        return 1-input;
+    }
+*/
+
+/* approch2
+    function flip(input){
+        return -(input-1);
+    }
+
+*/
+/* approch3
+    function flip (input){
+        return (input+1)%2;
+    }
+*/
