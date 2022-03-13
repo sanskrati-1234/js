48637c5c1cd24b6a5b2de1adb2719a9dc8dc48ad
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-03-13 20:04:43 +0530

    DeepCopy

--- CustomDeepCopy.js
+++ CustomDeepCopy.js
@@ -1,23 +1,46 @@
-const obj={
-    name:"Sanskrati",
-    details:{
-        address:"Patan",
-        phone:{
-            number1:"9522751076",
-            number2:"9090909090"
-        }
+const obj = {
+  name: "Sanskrati",
+  details: {
+    address: "Patan",
+    phone: {
+      number2: {
+        xyz: {
+          zx: "abc",
+        },
+      },
+    },
+  },
+  image: {
+    image1: "11",
+    image2: {
+      key1: {
+        key2: "xyz",
+        key3: {
+          key4: [1, 2, 3, 4],
+        },
+      },
+    },
+  },
+};
+
+const deepCopy = (obj) => {
+  if (!Boolean(obj)) {
+    return;
+  }
+  let newObj = {};
+  for (let item in obj) {
+    if (typeof obj[item] !== "object") {
+      newObj[item] = obj[item];
+    } else if (typeof obj[item] === "object") {
+      newObj[item] = deepCopy(obj[item]);
     }
-}
-let obj1;
-function deepCopy(obj){
-    let newObj={};
-    for(let item in obj){
-        if(typeof item != obj)
-            newObj[item]=obj.item;
-        if(typeof item ==="object"){
-            newObj[item]=deepCopy(item)
-        }
-        console.log(item);
-    }
-}
-deepCopy(obj)
+  }
+
+  return newObj;
+};
+
+obj1 = deepCopy(obj);
+obj1.details.address = "Jabalpur";
+console.log(obj1);
+console.log(obj1.details.address);
+console.log(obj.details.address);
